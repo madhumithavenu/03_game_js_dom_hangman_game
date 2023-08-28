0bd818cbbac22d623e8fd1f5096bb7a7a35f5be2
Madhumitha <madhumithavenu03@gmail.com>
2023-08-28 21:40:53 +0530

    Creating Js file

---
+++ script.js
@@ -0,0 +1,11 @@
+const keyboardDiv = document.querySelector(".keyboard");
+
+const getRandomWord = () =>{
+
+}
+//Creating keyboard buttons
+for (let i=97;i <= 122; i++){
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+}
